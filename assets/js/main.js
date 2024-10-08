9f7b1dfa1c602cfddc568464c21559a4edad57b6
Efestus31 <biagiohd@gmail.com>
2024-10-08 14:30:57 +0200

    pseudo codice

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+//selezioniamo gli elementi nel documento tramite id per poterli manipolare
+
+//funzione posta a true per poter ripetere l'evento
+
+
+//cerchiamo una funzione in caso di evento di click del pulsante
+
+        //codizione che viene posta a vero e quindi cambia src dell'immagine e l'inner html del bottone
+        
+        //se falsa cambia src dell'immagine e l'inner html del bottone
+        
